722452ac0313e6e5ed3545194215b909091856d4
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2023-02-21 20:56:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Results from "./Results";
 
-function App() {
+const synth = window.speechSynthesis;
+
+const App = () => {
+  console.log(synth.getVoices());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>English Dictonary</h1>
+
+      <form>
+        <div className="row">
+          <textarea cols="30" rows="4" placeholder="Enter Text" />
+
+          <div className="voices-icons">
+            <div className="select-voices">
+              <select>
+                <option value="">English</option>
+                <option value="">English</option>
+                <option value="">English</option>
+              </select>
+            </div>
+            <i className="fa-solid fa-volume-high" />
+          </div>
+        </div>
+      </form>
+      <Results />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Results.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Results = () => {
+  return (
+    <ul>
+      <li className="word">
+        <h2>Hello</h2>
+        <span>/həˈləʊ,hɛˈləʊ/</span>
+      </li>
+      <li className="contain">
+        <h3>noun</h3>
+        <div className="details">
+          <h3>Meaning</h3>
+          <p>-used as a greeting or to begin a phone conversation</p>
+        </div>
+
+        <div className="synonym">
+          <h3>Synonyms</h3>
+          <span>greeting,</span>
+          <span>greeting,</span>
+          <span>greeting</span>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default Results;
